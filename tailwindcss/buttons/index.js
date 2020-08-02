c6b7a111493479b948019203f426192adb6f7f00
Andy <andy@flowpros.com>
2020-08-03 01:34:23 +0200

    Updates

--- tailwindcss/buttons/index.js
+++ tailwindcss/buttons/index.js
@@ -1,5 +1,6 @@
 const _ = require("lodash");
 const Color = require("color");
+const { transparent } = require("../extend/colors");
 
 module.exports = function (options) {
   const { addComponents, theme, e } = options;
@@ -21,20 +22,33 @@ module.exports = function (options) {
       ".btn-sm": {
         "@apply px-3 py-2 text-sm": {},
       },
-    },
-    {
       ".btn-xs": {
         "@apply px-2 py-1 text-xs": {},
       },
-    },
-    {
       ".btn-lg": {
         "@apply px-5 py-4 text-lg": {},
       },
-    },
-    {
       ".btn-xl": {
         "@apply px-6 py-5 text-lg": {},
+      },
+      // Icons
+      ".btn-icon": {
+        "@apply inline-flex items-center": {},
+      },
+      ".btn-icon.btn-xs svg": {
+        "@apply wh-4": {},
+      },
+      ".btn-icon.btn-sm svg": {
+        "@apply wh-4": {},
+      },
+      ".btn-icon svg": {
+        "@apply wh-5": {},
+      },
+      ".btn-icon.btn-lg svg": {
+        "@apply wh-6": {},
+      },
+      ".btn-icon.btn-xl svg": {
+        "@apply wh-8": {},
       },
     },
     ..._.map(colors, (colorOptions, name) => {
@@ -45,6 +59,13 @@ module.exports = function (options) {
         bgColor = colorOptions[500];
       } else {
         bgColor = colorOptions;
+      }
+
+      let softBgColor = "";
+      if (_.isObject(colorOptions)) {
+        softBgColor = colorOptions[50];
+      } else {
+        softBgColor = colorOptions;
       }
 
       let textColor = "";
@@ -73,6 +94,61 @@ module.exports = function (options) {
             ),
           },
           "&:active": {
+            backgroundColor: _.get(
+              colorOptions,
+              "activeBackground",
+              Color(bgColor).darken(0.1).hex().toString()
+            ),
+          },
+        },
+        [`.btn-outline-${e(name)}`]: {
+          color: bgColor,
+          borderColor: bgColor,
+          backgroundColor: "transparent",
+          "&:focus": {
+            outline: 0,
+            boxShadow: `0 0 0 .2em ${Color(textColor)
+              .alpha(0.5)
+              .rgb()
+              .toString()}`,
+          },
+          "&:hover": {
+            color: textColor,
+            backgroundColor: _.get(
+              colorOptions,
+              "hoverBackground",
+              Color(bgColor).darken(0.1).hex().toString()
+            ),
+          },
+          "&:active": {
+            color: textColor,
+            backgroundColor: _.get(
+              colorOptions,
+              "activeBackground",
+              Color(bgColor).darken(0.1).hex().toString()
+            ),
+          },
+        },
+        [`.btn-${e(name)}-soft`]: {
+          color: bgColor,
+          backgroundColor: softBgColor,
+          "&:focus": {
+            outline: 0,
+            boxShadow: `0 0 0 .2em ${Color(bgColor)
+              .alpha(0.5)
+              .rgb()
+              .toString()}`,
+          },
+          "&:hover": {
+            color: textColor,
+            backgroundColor: _.get(
+              colorOptions,
+              "hoverBackground",
+              Color(bgColor).darken(0.1).hex().toString()
+            ),
+          },
+          "&:active": {
+            color: textColor,
             backgroundColor: _.get(
               colorOptions,
               "activeBackground",
