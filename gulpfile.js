79f95d55fa3522d8fa368d02e0ac0e4e73b32a1c
Andy <andy@flowpros.com>
2021-05-29 16:36:57 +0100

    Minified TW

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ const { watch, series, src, dest } = require("gulp");
 var concat = require("gulp-concat");
 var browserSync = require("browser-sync").create();
 var postcss = require("gulp-postcss");
-var svgcss = require("gulp-svg-css");
-var svgmin = require("gulp-svgmin");
+// var svgcss = require("gulp-svg-css");
+// var svgmin = require("gulp-svgmin");
 
 // Task for compiling our CSS files using PostCSS
 function cssTask(cb) {
@@ -16,35 +16,75 @@ function cssTask(cb) {
   cb();
 }
 
-function svgZWTask(cb) {
-  return src("./src/assets/svg/zwicons/**/*.svg")
-    .pipe(svgmin())
-    .pipe(
-      svgcss({
-        fileName: "zw-icons",
-        cssPrefix: "zwicon-",
-        addSize: false,
-      })
-    )
-    .pipe(dest("./src/css"))
-    .pipe(dest("dist/css"));
-  cb();
-}
+// Example outputting svgs to css - decided not to use anymore
+// function svgCIBTask(cb) {
+//   return src("./src/assets/svg/core-ui-brand/*.svg")
+//     .pipe(svgmin())
+//     .pipe(
+//       svgcss({
+//         fileName: "cib-icons",
+//         cssPrefix: "cib-",
+//         addSize: false,
+//       })
+//     )
+//     .pipe(dest("./src/css"))
+//     .pipe(dest("dist/css"));
+//   cb();
+// }
 
-function svgCIBTask(cb) {
-  return src("./src/assets/svg/core-ui-brand/*.svg")
-    .pipe(svgmin())
-    .pipe(
-      svgcss({
-        fileName: "cib-icons",
-        cssPrefix: "cib-",
-        addSize: false,
-      })
-    )
-    .pipe(dest("./src/css"))
-    .pipe(dest("dist/css"));
-  cb();
-}
+// Use to optomize svgs once off not part of build process
+// Removes dimensions and adds wh-x class and fill currentcolor
+// Icons added to iconjar
+// const plugins = [
+//   {
+//     addClassesToSVGElement: {
+//       className: "wh-6",
+//     },
+//   },
+//   {
+//     addAttributesToSVGElement: {
+//       attributes: [{ fill: "currentColor" }],
+//     },
+//   },
+//   {
+//     removeDimensions: true,
+//   },
+//   { removeAttrs: { attrs: "data-name" } },
+// ];
+
+// function svgZWTask(cb) {
+//   return src("./src/assets/svg/zwicons/**/*.svg")
+//     .pipe(
+//       svgmin({
+//         plugins,
+//       })
+//     )
+
+//     .pipe(dest("./src/assets/svg/optimized/zw-icons"));
+//   cb();
+// }
+
+// function svgCIBTask(cb) {
+//   return src("./src/assets/svg/core-ui-brand/*.svg")
+//     .pipe(
+//       svgmin({
+//         plugins,
+//       })
+//     )
+//     .pipe(dest("./src/assets/svg/optimized/core-ui-brand"));
+//   cb();
+// }
+
+// function svgUniconsTask(cb) {
+//   return src("./src/assets/svg/unicons-line/*.svg")
+//     .pipe(
+//       svgmin({
+//         plugins,
+//       })
+//     )
+//     .pipe(dest("./src/assets/svg/optimized/unicons-line"));
+//   cb();
+// }
 
 function htmlTask(cb) {
   return src("./src/index.html") // read .css files from ./src/ folder
@@ -68,13 +108,14 @@ function browsersyncReload(cb) {
 
 // Watch Files & Reload browser after tasks
 function watchTask() {
-  watch(".src/*.html", browsersyncReload);
-  watch([".src/*.css"], series(cssTask, browsersyncReload));
+  watch("./**/*.html", browsersyncReload);
+  watch(["./src/*.css"], series(cssTask, browsersyncReload));
 }
 
 // Default Gulp Task
-exports.default = series(cssTask, htmlTask, browsersyncServe, watchTask);
+exports.default = series(cssTask, browsersyncServe, watchTask);
 exports.css = cssTask;
-exports.html = htmlTask;
-exports.svgZw = svgZWTask;
-exports.svgCib = svgCIBTask;
+exports.htmlTask = htmlTask;
+// exports.svgZw = svgZWTask;
+// exports.svgCib = svgCIBTask;
+// exports.svgUnicons = svgUniconsTask;
