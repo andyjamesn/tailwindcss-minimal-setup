9adca008acbb5b19eb700e278e0ee0426d5ee1a8
Andy <andy@flowpros.com>
2021-05-29 17:22:02 +0100

    Minified TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -144,7 +144,6 @@ module.exports = {
     wordBreak: false,
     zIndex: false,
   },
-
   theme: {
     screens: {
       'sm': '478px',
@@ -158,6 +157,34 @@ module.exports = {
 
       'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
+    },
+    spacing: {
+      px: '1px',
+      0: '0px',
+      0.5: '0.125rem',
+      1: '0.25rem',
+      1.5: '0.375rem',
+      2: '0.5rem',
+      2.5: '0.625rem',
+      3: '0.75rem',
+      3.5: '0.875rem',
+      4: '1rem',
+      5: '1.25rem',
+      6: '1.5rem',
+      7: '1.75rem',
+      8: '2rem',
+      9: '2.25rem',
+      10: '2.5rem',
+      11: '2.75rem',
+      12: '3rem',
+      14: '3.5rem',
+      16: '4rem',
+      20: '5rem',
+      24: '6rem',
+      28: '7rem',
+      32: '8rem',
+      36: '9rem',
+      40: '10rem',
     },
     extend: {
       colors: {
